6231075df6a59cd200f307208d07f2501f990859
TAIGA <taigamiura22@gmail.com>
2020-09-25 13:31:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const ERR_MSG = '未入力項目または不正な文字入力があります'
 const LINE = '======================='
 
 const ITEM_LIST = [
-  'period'                   // 第何期
-  , 'ryudou_shisan'          // 流動資産
-  , 'kotei_shisan'           // 固定資産
-  , 'total_shisan'           // 資産合計(流動資産 + 固定資産)
-  , 'ryudou_husai'           // 流動負債
-  , 'kabunushi_shihon'       // 株主資本
-  , 'shihon_kin'             // 資本金
-  , 'shihon_yozyoukin'       // 資本余剰金
-  , 'shihon_zyunbikin'       // 資本準備金
-  , 'rieki_yozyoukin'        // 利益剰余金
-  , 'sonohokarieki_yozyoukin'// その他利益余剰金
-  , 'uti_touki_zyunrieki'    // (うち当期純利益)
-  , 'total_husai_jyunshisan' // 純資産及び負債の合計()
+  'period'                             // 第何期
+  , 'current_assets'                   // 流動資産
+  , 'fixed_asset'                      // 固定資産
+  , 'total_assets'                     // 資産合計(流動資産 + 固定資産)
+  , 'current_Liabilities'              // 流動負債
+  , 'shareholders_equity'              // 株主資本
+  , 'capital'                          // 資本金
+  , 'capital_surplus'                  // 資本余剰金
+  , 'capital_reserve'                  // 資本準備金
+  , 'retained_earnings'                // 利益剰余金
+  , 'other_retained_earnings'          // その他利益余剰金
+  , 'net_income'                       // (うち当期純利益)
+  , 'total_net_assets_and_liabilities' // 純資産及び負債の合計()
 ]
 
 const ITEM_HASH = {
@@ -36,7 +36,11 @@ const ITEM_HASH = {
   ]
 }
 
-const FIRLD_LIST = [ '科目','金額(千円)','金額(千円)' ]
+const FIRLD_LIST = [
+  '科目'
+  , '科目'
+  , '金額(千円)'
+]
 
 window.onload = () => {
   let result = `<form name="form" action="main.html">`
@@ -81,28 +85,28 @@ window.onload = () => {
 
 const btn_click = () => {
   let value_list = [
-    form.period.value
-    , form.ryudou_shisan.value
-    , form.kotei_shisan.value
-    , form.total_shisan.value
-    , form.ryudou_husai.value
-    , form.kabunushi_shihon.value
-    , form.shihon_kin.value
-    , form.shihon_yozyoukin.value
-    , form.shihon_zyunbikin.value
-    , form.rieki_yozyoukin.value
-    , form.sonohokarieki_yozyoukin.value
-    , form.uti_touki_zyunrieki.value
-    , form.total_husai_jyunshisan.value
+    form.period.value                             // 第何期
+    , form.current_assets.value                   // 流動資産
+    , form.fixed_asset.value                      // 固定資産
+    , form.total_assets.value                     // 資産合計(流動資産 + 固定資産)
+    , form.current_Liabilities.value              // 流動負債
+    , form.shareholders_equity.value              // 株主資本
+    , form.capital.value                          // 資本金
+    , form.capital_surplus.value                  // 資本余剰金
+    , form.capital_reserve.value                  // 資本準備金
+    , form.retained_earnings.value                // 利益剰余金
+    , form.other_retained_earnings.value          // その他利益余剰金
+    , form.net_income.value                       // (うち当期純利益)
+    , form.total_net_assets_and_liabilities.value // 純資産及び負債の合計()
   ]
 
   value_list = value_list.map((v, index) => parseInt(v, 10))
 
-  let input_check = 0
+  let blank_num = 0
   // forEachではbreakできないためこの実装にしている。
   for (let index = 0; index < value_list.length; index++) {
     if (isNaN(value_list[index])) {
-      input_check++
+      blank_num++
       alert(ERR_MSG)
       break;
     } else {
@@ -110,7 +114,7 @@ const btn_click = () => {
     }
   }
 
-  if (input_check === 0) {
+  if (blank_num === 0) {
     /* 売上高 = 「総資産 × １倍」 */
     const AMOUNT_OF_SALES = (value_list[1] + value_list[2]) * 1
     /* 「過去の平均的な年間利益」=「（利益剰余金 − 当期純利益）÷（当期の期数 −１）」 */
@@ -126,12 +130,12 @@ const btn_click = () => {
     /* 棚卸資産 ＝ 売上高 ÷ 12ヶ月 × １ヶ月 */
     const INVENTORY = parse_int_calc(1)
 
-    console.log('売上高:' + AMOUNT_OF_SALES)
-    console.log('過去の平均的な年間利益:' + KEYAVG_ANNUAL_PROFIT_IN_THE_PAST)
-    console.log('流動比率:' + CURRENT_RATIO)
-    console.log('現金・預金:' + CASH_AND_DEPOSIT)
-    console.log('売掛金:' + ACCOUNTS_RECEIVABLE)
-    console.log('棚卸資産:' + INVENTORY)
+    console.log(`売上高:${AMOUNT_OF_SALES}`)
+    console.log(`過去の平均的な年間利益:${KEYAVG_ANNUAL_PROFIT_IN_THE_PAST}`)
+    console.log(`流動比率:${CURRENT_RATIO}`)
+    console.log(`現金・預金:${CASH_AND_DEPOSIT}`)
+    console.log(`売掛金:${ACCOUNTS_RECEIVABLE}`)
+    console.log(`棚卸資産:${INVENTORY}`)
 
     console.log(LINE)
     console.log('「過去の平均利益」')
@@ -139,7 +143,7 @@ const btn_click = () => {
       console.log('-> 当期は不調気味？')
     } else if ((value_list.length - 2 / value_list.length - 1) * 100 < 1){
       console.log('-> 「粉飾決算の可能性」もアタマに入れておくとよいでしょう。')
-    }else {
+    } else {
       console.log('-> おおむね問題なし')
     }
 
