941e5c2b7b278c59b8479f71c8e22850230003bc
masayaHb <hm2kk9586@icloud.com>
2020-09-28 01:41:28 +0900

    類似処理を一番下のeache()にまとめました

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const ERR_MSG = '未入力項目または不正な文字入力があります'
-const LINE = '======================='
-let MESSAGE
-let CONDITION
-let PROBLEM
+const LINE    = '======================='
 
 const ITEM_LIST = [
   'period'                             // 第何期
   , 'current_assets'                   // 流動資産
   , 'fixed_asset'                      // 固定資産
   , 'total_assets'                     // 資産合計(流動資産 + 固定資産)
-  , 'current_Liabilities'              // 流動負債
+  , 'current_liabilities'              // 流動負債
   , 'shareholders_equity'              // 株主資本
   , 'capital'                          // 資本金
   , 'capital_surplus'                  // 資本余剰金
@@ -38,45 +35,30 @@ const ITEM_HASH = {
     , '合計'
   ]
 }
-// console.log(ITEM_HASH['資産の部'][0]);
+
 const FIRLD_LIST = [
   '科目'
   , '科目'
   , '金額(千円)'
 ]
+//下記の処理は、類似処理を関数[eache()]で作成したためHTML文を入れる変数を用意
+let result
 
 window.onload = () => {
-  let result = `<form name="form" action="main.html">`
-      result += `<h1 id="${ITEM_LIST[0]}">第<input type="text" size="1" name="${ITEM_LIST[0]}">期決算公告</h1>`
-      result += `<table border="1" width="450" >`
-      result += `<tr>`
-      FIRLD_LIST.forEach(element => result += `<th>${element}</th>`)
-      result += `</tr>`
+  result = `<form name="form" action="main.html">`
+  result += `<h1 id="${ITEM_LIST[0]}">第<input type="text" size="1" name="${ITEM_LIST[0]}">期決算公告</h1>`
+  result += `<table border="1" width="450" >`
+  result += `<tr>`
+  FIRLD_LIST.forEach(element => result += `<th>${element}</th>`)
+  result += `</tr>`
 
   let i = 1
   for (const KEY in ITEM_HASH) {
     if (KEY === '資産の部') {
-      ITEM_HASH[KEY].forEach((element, index) => {
-        if (index === 0) {
-          result += `<tr>`
-          result += `<td rowspan="${ITEM_HASH[KEY].length}"><b>${KEY}</b></td>`
-        }
-        result += `<td>${element}</td>`
-        result += `<td id="${ITEM_LIST[i]}" width="100"><input type="text" name="${ITEM_LIST[i]}"></td>`
-        result += `</tr>`
-        i++
-      })
+      eache(KEY,i)
+      i += KEY.length -1
     } else if (KEY === '負債及び純資産の部') {
-      ITEM_HASH[KEY].forEach((element, index) => {
-        if (index === 0) {
-            result += `<tr>`
-            result += `<td rowspan="${ITEM_HASH[KEY].length}"><b>${KEY}</b></td>`
-        }
-        result += `<td>${element}</td>`
-        result += `<td id="${ITEM_LIST[i]}"width="100"><input type="text" name="${ITEM_LIST[i]}"></td>`
-        result += `</tr>`
-        i++
-      })
+      eache(KEY,i)
     }
   }
   result += `</table>`
@@ -91,8 +73,7 @@ const btn_click = () => {
     form.period.value                             // 第何期
     , form.current_assets.value                   // 流動資産
     , form.fixed_asset.value                      // 固定資産
-    , form.total_assets.value                     // 資産合計(流動資産 + 固定資産)
-    , form.current_Liabilities.value              // 流動負債
+    , form.current_liabilities.value              // 流動負債
     , form.shareholders_equity.value              // 株主資本
     , form.capital.value                          // 資本金
     , form.capital_surplus.value                  // 資本余剰金
@@ -100,36 +81,39 @@ const btn_click = () => {
     , form.retained_earnings.value                // 利益剰余金
     , form.other_retained_earnings.value          // その他利益余剰金
     , form.net_income.value                       // (うち当期純利益)
-    , form.total_net_assets_and_liabilities.value // 純資産及び負債の合計()
   ]
 
-  for(let i = 0 ; i < value_list.length ; i ++){
-          if (value_list[i].match(/[０-９]/)){
-              value_list[i]= check(value_list[i])
-            }
+  value_list = value_list.map(element => {
+    if (element.match(/[０-９]/)) {
+      element = replace_char_code(element)
+    }
+    return parseInt(element)
+  })
+
+  // '資産の部'の合計を追加
+  value_list.splice(3,0,value_list[1] + value_list[2])
+
+  // '負債及び純資産の部'の合計を追加
+  let num = 0;
+  for(let l = 4; l < value_list.length; l++){
+    num += value_list[l]
   }
+  value_list.push(num)
 
-  function check(elm) {
-    return elm.replace(/[０-９]/g, function(s){
-        return String.fromCharCode(s.charCodeAt(0)-0xFEE0);
-    });
-}
-
-  value_list = value_list.map(v => parseInt(v, 10))
-  console.log(value_list.map(v => parseInt(v, 10)));
-  let blank_num = 0
+  let blank_flag = true
   // forEachではbreakできないためこの実装にしている。
   for (let index = 0; index < value_list.length; index++) {
     if (isNaN(value_list[index])) {
-      blank_num++
+      blank_flag = false
       alert(ERR_MSG)
       break
-    } else {
-      document.getElementById(ITEM_LIST[index]).innerHTML = value_list[index]
     }
+
+    let tmp = (index === 0) ? `第${value_list[index]}決算広告` : value_list[index]
+    document.getElementById(ITEM_LIST[index]).innerHTML = tmp
   }
 
-  if (blank_num === 0) {
+  if (blank_flag) {
     /* 売上高 = 「総資産 × １倍」 */
     const AMOUNT_OF_SALES = (value_list[1] + value_list[2]) * 1
     /* 「過去の平均的な年間利益」=「（利益剰余金 − 当期純利益）÷（当期の期数 −１）」 */
@@ -139,43 +123,13 @@ const btn_click = () => {
 
     const parse_int_calc = num => parseInt((AMOUNT_OF_SALES / 12 * num), 10)
     /* 現金・預金 ＝ 売上高 ÷ 12ヶ月 × 1.5ヶ月 */
-    const CASH_AND_DEPOSIT = parse_int_calc(1.5)
+    const CASH_AND_DEPOSIT    = parse_int_calc(1.5)
     /* 売掛金 ＝ 売上高 ÷ 12ヶ月 × ２ヶ月 */
     const ACCOUNTS_RECEIVABLE = parse_int_calc(2)
     /* 棚卸資産 ＝ 売上高 ÷ 12ヶ月 × １ヶ月 */
-    const INVENTORY = parse_int_calc(1)
+    const INVENTORY           = parse_int_calc(1)
 
-//ここから
-    if (KEYAVG_ANNUAL_PROFIT_IN_THE_PAST < value_list.length - 2) {
-      console.log('-> 当期は不調気味？')
-      MESSAGE = '-> 当期は不調気味？'
-    } else if ((value_list.length - 2 / value_list.length - 1) * 100 < 1){
-      console.log('-> 「粉飾決算の可能性」もアタマに入れておくとよいでしょう。')
-      MESSAGE = '-> 「粉飾決算の可能性」もアタマに入れておくとよいでしょう。'
-    } else {
-      console.log('-> おおむね問題なし')
-      MESSAGE = '-> おおむね問題なし'
-    }
-    /* 流動比率は200%以上が望ましく、100%未満はマズイ */
-    if (CURRENT_RATIO >= 200) {
-      console.log('-> Good')
-      CONDITION = '-> Good'
-    } else if (CURRENT_RATIO < 100) {
-      console.log('-> Bad')
-      CONDITION = '-> Bad'
-    } else {
-      console.log('-> おおむね問題なし')
-      CONDITION = '-> おおむね問題なし'
-    }
-    if ((CASH_AND_DEPOSIT + ACCOUNTS_RECEIVABLE + INVENTORY) < value_list[2]) {
-      console.log('-> 流動資産のなかに架空資産や不良資産が混じっている可能性あり')
-      PROBLEM = '-> 流動資産のなかに架空資産や不良資産が混じっている可能性あり'
-    } else {
-      console.log('-> おおむね問題なし')
-      PROBLEM = '-> おおむね問題なし'
-    }
-
-    const RESULT_LIST = [
+    const result_list = [
       `売上高:${AMOUNT_OF_SALES}`
       , `過去の平均的な年間利益:${KEYAVG_ANNUAL_PROFIT_IN_THE_PAST}`
       , `流動比率:${CURRENT_RATIO}`
@@ -184,23 +138,66 @@ const btn_click = () => {
       , `棚卸資産:${INVENTORY}`
       , LINE
       , '「過去の平均利益」'
-      , MESSAGE
-      , LINE
-      , '「流動比率」'
-      , CONDITION
-      , LINE
-      , 'やばい会社かどうか'
-      , PROBLEM
     ]
-    //ここまで
+
+    if (KEYAVG_ANNUAL_PROFIT_IN_THE_PAST < value_list.length - 2) {
+      result_list.push('-> 当期は不調気味？')
+    } else if ((value_list.length - 2 / value_list.length - 1) * 100 < 1){
+      result_list.push('-> 「粉飾決算の可能性」もアタマに入れておくとよいでしょう。')
+    } else {
+      result_list.push('-> おおむね問題なし')
+    }
+
+    /* 流動比率は200%以上が望ましく、100%未満はマズイ */
+    result_list.push(LINE)
+    if (CURRENT_RATIO >= 200) {
+      result_list.push('-> Good')
+    } else if (CURRENT_RATIO < 100) {
+      result_list.push('-> Bad')
+    } else {
+      result_list.push('-> おおむね問題なし')
+    }
+
+    result_list.push(LINE)
+    if ((CASH_AND_DEPOSIT + ACCOUNTS_RECEIVABLE + INVENTORY) < value_list[2]) {
+      result_list.push('-> 流動資産のなかに架空資産や不良資産が混じっている可能性あり')
+    } else {
+      result_list.push('-> おおむね問題なし')
+    }
 
     let html = `<div></div>`
-        html += `<ul type="disc">`
-        RESULT_LIST.forEach(element => {
-          html += `<li>${element}</li>`
-        })
-        html += `</ul>`
-        console.log(html);
-        document.getElementById('add').innerHTML = html
+    html += `<ul type="disc">`
+    result_list.forEach(element => html += `<li>${element}</li>`)
+    html += `</ul>`
+    document.getElementById('add').innerHTML = html
   }
+}
+
+// 全角文字があれば半角に変換する
+const replace_char_code = element => {
+  return element.replace(/[０-９]/g, s => {
+    return String.fromCharCode(s.charCodeAt(0)-0xFEE0)
+  })
+}
+
+const eache = (KEY,i) => {
+  ITEM_HASH[KEY].forEach((element, index) => {
+    if (index === 0) {
+      result += `<tr>`
+      result += `<td rowspan="${ITEM_HASH[KEY].length}"><b>${KEY}</b></td>`
+    }
+    result += `<td>${element}</td>`
+    result += `<td id="${ITEM_LIST[i]}"width="100">`
+    if(KEY === '資産の部'){
+      if (i !== KEY.length -1){
+        result += `<input type="text" name="${ITEM_LIST[i]}">`
+      }
+    }else if(KEY === '負債及び純資産の部'){
+      if(i !== ITEM_HASH['資産の部'].length + ITEM_HASH['負債及び純資産の部'].length){
+        result += `<input type="text" name="${ITEM_LIST[i]}">`
+      }
+    }
+    result += `</td></tr>`
+    i++
+  })
 }
